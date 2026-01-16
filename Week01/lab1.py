c90ad5dc25c9d3ceb7c57504f91de7267c897a25
Dann1706 <dann1706@icloud.com>
2026-01-16 12:48:26 -0500

    Add Lab 01

---
+++ Week01/lab1.py
@@ -0,0 +1,29 @@
+# Sample Coding Question 01 - Week 01
+# Daniel Alvarez
+
+
+# Question 02 : Defining an array (list)
+my_array = [1, 4, 7, 9]
+
+
+# Question 3 : Order of Operations
+a = 1
+b = 2
+c = 3
+d = 4
+
+
+# Fully-Bracketed Version of : e = a - b ** c // d + a % c
+e = (a - ((b ** c) // d)) + (a % c)
+print("Question 3 result is: ", e)
+
+
+# Question 4: Formatting
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+
+# Question 5: User Input
+userAge = int(input("Enter your age: "))
+userAge = userAge + 22
+print("Your new age is: ", userAge)
